ea25814f5d100072c3bf36a93b678eabf3b04640
slicycode <deparisjules180@gmail.com>
2023-05-09 21:37:35 +0200

    fixed build error

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -9,7 +9,11 @@ const PromptCardList = ({ data, handleTagClick }: PromptCardListProps) => {
   return (
     <div className="mt-16 prompt_layout">
       {data.map((post) => (
-        <PromptCard key={post.id} post={post} handleTagClick={handleTagClick} />
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
       ))}
     </div>
   );
